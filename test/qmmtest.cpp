6d756ea09b81ae0576bdb5305088874049008cdc
Charlie Martin <martinc@ucar.edu>
2011-05-15 03:31:31 +0000

    Add capability to pas additional command line parameters to QApplication. This allows "-graphicssystem raster" or "-graphicssystem opengl" to QApplication. This can drastically speed up the rendering on X11 display systems which don't have hardware graphics acceleration enabled. Yes, there are some such systems out there, such as VMWare fusion for instance.

--- test/qmmtest.cpp
+++ test/qmmtest.cpp
@@ -5,32 +5,78 @@
  *      Author: martinc
  */
 
+#include <unistd.h>
 #include <iostream>
 #include <string>
 #include <vector>
 #include "QMicroMapTest.h"
 
-int main(int argc, char** argv) {
-	if (argc != 2 && argc != 6) {
-		std::cerr <<"usage: " << argv[0] << " db_path [xmin ymin xmax ymax]" << std::endl;
-		exit(1);
+void options(int argc, char**argv,
+		std::string& dbpath,
+		double& xmin,
+		double& xmax,
+		double& ymin,
+		double& ymax) {
+
+	extern char *optarg;
+	int opt;
+	bool err = false;
+
+	while ((opt = getopt(argc, argv, "b:d:")) != -1) {
+		switch (opt) {
+		case 'b': {
+			std::string arg(optarg);
+			std::vector<std::string> tokens;
+			std::string::size_type lastPos = arg.find_first_not_of(",", 0);
+			std::string::size_type pos     = arg.find_first_of(",", lastPos);
+
+			while (std::string::npos != pos || std::string::npos != lastPos)
+			{
+				tokens.push_back(arg.substr(lastPos, pos - lastPos));
+				lastPos = arg.find_first_not_of(",", pos);
+				pos     = arg.find_first_of(",", lastPos);
+			}
+			if (tokens.size() != 4) {
+				err = true;
+				break;
+			}
+			xmin = atof(tokens[0].c_str());
+			ymin = atof(tokens[1].c_str());
+			xmax = atof(tokens[2].c_str());
+			ymax = atof(tokens[3].c_str());
+			break;
+		}
+		case 'd':
+			dbpath = std::string(optarg);
+			break;
+		default:
+			break;
+		}
 	}
 
-	// Get the geographic database
-	std::string dbpath = std::string(argv[1]);
-	SpatiaLiteDB db(dbpath);
+	if (dbpath.size() == 0) {
+		err = true;
+	}
 
+	if (err) {
+		std::cerr <<"usage: " << argv[0] << " -d db_path [-b xmin,ymin,xmax,ymax] [qt args]" << std::endl;
+		exit(1);
+	}
+}
+
+int main(int argc, char** argv) {
+
+	std::string dbpath;
 	double xmin = -180.0;
 	double ymin =  -90.0;
 	double xmax = 180.0;
 	double ymax =  90.0;
 
-	if (argc == 6) {
-		xmin = atof(argv[2]);
-		ymin = atof(argv[3]);
-		xmax = atof(argv[4]);
-		ymax = atof(argv[5]);
-	}
+	options(argc, argv, dbpath, xmin, ymin, xmax, ymax);
+
+	// get the database
+	SpatiaLiteDB db(dbpath);
+
 
 	// Create a Qt application
 	QApplication app(argc, argv);
