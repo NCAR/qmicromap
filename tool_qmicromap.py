2bcdb83f8d5117af4916d9bc186d3216898bc5b0
isabels <isabels@ucar.edu>
2025-04-10 14:40:09 -0600

    Remove obsolete win_qt_setup() from tool_qmicromap. While I'm at it, also remove mac_qt_setup(), which no longer does anything

--- tool_qmicromap.py
+++ tool_qmicromap.py
@@ -6,23 +6,6 @@ env = Environment(tools=['default'] + tools)
 qttools = env.Split('qtcore qtgui qtsvg')
 env.Require(qttools)
 
-
-def win_qt_setup(env):
-    # Windows needs an extra include path for Qt modules.
-    qt5include = env['QT5DIR'] + '/include'
-    env.AppendUnique(CPPPATH=[qt5include])
-    env.Require(qttools)
-
-def mac_qt_setup(env):
-    # Mac OS setup
-    # Qt configuration:
-    #    the following uses the frameworks scheme available for gcc on Mac OS
-    #    to provide libraries and library paths
-    #frameworkpath='/usr/local/lib'
-    #env.AppendUnique(FRAMEWORKPATH=[frameworkpath,])
-    #env.AppendUnique(FRAMEWORKS=qt4Modules)
-    pass
-
 libsources = Split("""
   QMicroMap.cpp
   QStationModelGraphicsItem.cpp
@@ -33,12 +16,6 @@ headers = Split("""
   QStationModelGraphicsItem.h
   MicroMapOverview.h
 """)
-
-if env['PLATFORM'] == 'darwin':
-    mac_qt_setup(env)
-
-if env['PLATFORM'] == 'win32':
-    win_qt_setup(env)
 
 libqmicromap = env.Library('qmicromap', libsources)
 env.Default(libqmicromap)
@@ -53,9 +30,5 @@ def qmicromap(env):
     env.Require(qttools)
     env.AppendUnique(CPPPATH=[thisdir])
     env.AppendDoxref('QMicroMap')
-    if env['PLATFORM'] == 'darwin':
-        mac_qt_setup(env)
-    if env['PLATFORM'] == 'win32':
-        win_qt_setup(env)
 
 Export('qmicromap')
